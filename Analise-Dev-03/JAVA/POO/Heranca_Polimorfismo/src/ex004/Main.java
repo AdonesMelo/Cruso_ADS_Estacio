5bf6d4d69c6002bd159366d87935d8d8151e77fd
Adones Melo <adones.n.m@outlook.com>
2026-08-04 19:10:13 -0300

    Pratica: Adcionado mais uma aluno

--- Analise-Dev-03/JAVA/POO/Heranca_Polimorfismo/src/ex004/Main.java
+++ Analise-Dev-03/JAVA/POO/Heranca_Polimorfismo/src/ex004/Main.java
@@ -5,6 +5,10 @@ public class Main {
     private static Endereco endereco;
     private static Data data;
 
+    private static Aluno aluno2;
+    private static Endereco endereco2;
+    private static Data data2;
+
     public static void main(String[] args) {
         // Criando uma data de nascimento
         data = new Data(9, 9, 1988); // 1 de Julho de 1988
@@ -23,6 +27,20 @@ public class Main {
         aluno = new Aluno("João", data.toCalendar(), 123456789L, endereco);
         aluno.atualizarIdade();
 
+        // Criando outro aluno
+        data2 = new Data(4, 5, 1988);
+        endereco2 = new Endereco();
+        endereco2.definirPais("Brasil");
+        endereco2.definirUF("SP");
+        endereco2.definirCidade("São Paulo");
+        endereco2.definirRua("Rua do Rio");
+        endereco2.definirNumero("100");
+        endereco2.definirCEP("12345678");
+        endereco2.definirComplemento("Sala 1");
+
+        aluno2 = new Aluno("Maria", data2.toCalendar(), 223456789L, endereco2);
+        aluno2.atualizarIdade();
+
         // Exibindo dados
         System.out.println("Aluno: " + aluno.recuperarNome());
         System.out.println("Idade: " + aluno.recuperarIdade());
@@ -30,5 +48,12 @@ public class Main {
         System.out.println("Data de Nascimento: " + data.getDataFormatada());
         System.out.println("Endereço:");
         aluno.recuperarEndereco().getDados();
+
+        System.out.println("\nAluno2: " + aluno2.recuperarNome());
+        System.out.println("Idade: " + aluno2.recuperarIdade());
+        System.out.println("CPF: " + aluno2.recuperarCPF());
+        System.out.println("Data de Nascimento: " + data2.getDataFormatada());
+        System.out.println("Endereço:");
+        aluno2.recuperarEndereco().getDados();
     }
 }

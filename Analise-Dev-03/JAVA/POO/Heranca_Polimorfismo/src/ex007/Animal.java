8badc567060d62d5a65127f36610c8ae28eb2b2d
Adones Melo <adones.n.m@outlook.com>
2026-08-06 16:34:42 -0300

    Desafio: Classes abstratas

--- Analise-Dev-03/JAVA/POO/Heranca_Polimorfismo/src/ex007/Animal.java
+++ Analise-Dev-03/JAVA/POO/Heranca_Polimorfismo/src/ex007/Animal.java
@@ -1,5 +1,9 @@
 package ex007;
 
+/*
+ Agora, o nosso desafio é implementar a classe “Leao” que também vai herdar da classe “Animal”, mas, além disso, ela deve ter um método “tipoDeAnimal” que imprima a mensagem: “É um animal selvagem”.
+*/
+
 // Classe abstrata
 abstract class Animal {
 
@@ -26,5 +30,22 @@ class Cachorro extends Animal {
     @Override
     public void emitirSom() {
         System.out.println("Au! Au!");
+    }
+}
+
+class Leao extends Animal {
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Grrrrr...");
+    }
+
+    public void tipoDeAnimal() {
+        System.out.println("É um animal selvagem");
+    }
+
+    @Override
+    public void dormir() {
+        System.out.println("Zzzzzzzz...");
     }
 }
--- Analise-Dev-03/JAVA/POO/Heranca_Polimorfismo/src/ex007/Main.java
+++ Analise-Dev-03/JAVA/POO/Heranca_Polimorfismo/src/ex007/Main.java
@@ -2,13 +2,23 @@ package ex007;
 
 public class Main {
     public static void main(String[] args) {
+        // Criação de objetos
         Animal gato = new Gato();
         Animal cachorro = new Cachorro();
+        Leao leao = new Leao();
 
+        // Invocação de métodos
+        // Gato
         gato.emitirSom();
         gato.dormir();
 
+        // Cachorro
         cachorro.emitirSom();
         cachorro.dormir();
+
+        // Leao
+        leao.emitirSom();
+        leao.tipoDeAnimal();
+        leao.dormir();
     }
 }
